8ed50a5061b313047828f5ee49155591753b1408
echodarkstar <nishantshankar226@gmail.com>
2017-05-12 10:37:29 +0530

    Creates a new csv with headers

--- dailydl.py
+++ dailydl.py
@@ -3,6 +3,8 @@ import urllib.request
 import re,sys,zipfile
 import schedule
 import time,os
+import csv
+from pandas import read_csv
 
 # def job():
 #Getting GDELT Event url content into a format that can be manipulated
@@ -27,11 +29,27 @@ def dlProgress(count, blockSize, totalSize):
 #Download today's file
 urllib.request.urlretrieve(fin_url, curr_file,reporthook=dlProgress)
 sys.stdout.write("\n")
+
+#Get column headers from website
+url = "http://gdeltproject.org/data/lookups/CSV.header.dailyupdates.txt"
+response = urllib.request.urlopen(url)
+data = response.read()      # a `bytes` object
+text = data.decode('utf-8') # a `str`; this step can't be used if data is binary
+
 #Extracting the zip file into csv
 zip = zipfile.ZipFile(curr_file)
 zip.extractall()
 #Remove zip file
 os.remove(curr_file)
+
+#Creating new csv with appropriate headers
+df = read_csv(curr_file.strip('.zip') , sep='\t', lineterminator='\n', header=None, low_memory=False)
+df.columns = text.split()
+os.remove(curr_file.strip('.zip'))
+df.to_csv(curr_file.strip('.zip'), sep='\t')
+
+#Bug : Actor/Action GEOID in csv is not clean.
+
 # schedule.every(5).minutes.do(job)
 # schedule.every().hour.do(job)
 # schedule.every().day.at("10:30").do(job)
